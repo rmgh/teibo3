fcab94db9b24e9af0f748929ee938b382f43eccd
rm <minowaryohei@minowa-no-MacBook-Air.local>
2015-05-12 11:55:00 +0900

    Add 3 cells in UITableView

--- teibo3/ViewController.swift
+++ teibo3/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var tableData: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        tableData = ["ハーゲンダッツ限定味 発売", "ゆで方のポイントから意外な調理法まで勢ぞろい。今が旬のアスパラガスを、心ゆくまで味わってみませんか？", "台風6号は奄美大島付近を北東に移動中"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int  {
+        return tableData.count
+    }
 
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+        let CellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier) as UITableViewCell
+        
+        let imageView = cell.viewWithTag(100) as UIImageView
+        imageView.image = UIImage(named: "\(indexPath.row+1).png")
+        
+        let textLabel = cell.viewWithTag(101) as UILabel
+        textLabel.text = tableData[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
+        tableData[indexPath.row] += tableData[indexPath.row]
+        tableView.reloadData()
+    }
 }
 
