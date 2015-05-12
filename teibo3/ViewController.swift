8c08e6f43638efe28398fa3271607421cb1108c1
rm <minowaryohei@minowa-no-MacBook-Air.local>
2015-05-12 09:55:45 +0900

    Initial Commit

---
+++ teibo3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  teibo3
+//
+//  Created by minowaryohei on 2015/05/12.
+//  Copyright (c) 2015年 rm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
